92d5ec2bc2367b291280ac9a99109e5f021b3cf0
Jayson Juanir <jaysonjuanir@yahoo.com>
2016-06-10 10:23:03 +0800

    backup of the hibernate example

--- src/main/java/hibernate_example/Main.java
+++ src/main/java/hibernate_example/Main.java
@@ -15,14 +15,30 @@ import org.hibernate.cfg.Configuration;
 public class Main {
 
     public static void main(String[] args) {
-        final Configuration configuration = new Configuration().configure();
+        
+		
+        
+		//USER INPUT = APP CLASS
+		final Book book = new Book("9781617291999 ", "Java 8 in Action", "Raoul-Gabriel Urma, Mario Fusco, and Alan Mycroft");
+        //END OF THE APP CLASS
+		
+		
+		//SERVICE PROCESSING V1
+		final Configuration configuration = new Configuration().configure();
         final StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
         final SessionFactory factory = configuration.buildSessionFactory(builder.build());
         final Session session = factory.openSession();
-        final Book book = new Book("9781617291999 ", "Java 8 in Action", "Raoul-Gabriel Urma, Mario Fusco, and Alan Mycroft");
-        session.beginTransaction();
+		//SERVICE CLASS CONTINUED	
+		
+		//DAO processing = DAO CLASS
+		session.beginTransaction();
         session.save(book);
         session.getTransaction().commit();
+		//I PLAN TO RETURN THE SESSION OBJECT BACK TO SERVICE
+		//END OF THE DAO CLASS
+		
+		
+		//SERVICE PROCESSING V2 WITH THE SESSION TAKEN BACK BY DAO CLASS 
         final List<Book> books = session.createCriteria(Book.class).list();
         System.out.println("\n----\n");
         System.out.println(MessageFormat.format("Storing {0} books in the database", books.size()));
@@ -31,6 +47,21 @@ public class Main {
         }
         System.out.println("\n----\n");
         session.close();
+        //factory.close();
+		//END OF THE SERVICE CLASS
+		
+		
+		//TESTING PURPOSE ONLY!
+        final Session session1 = factory.openSession();
+		
+		final List<Book> books1 = session1.createQuery("from Book").list();
+        System.out.println("\n----\n");
+        System.out.println(MessageFormat.format("Storing {0} books in the database", books1.size()));
+        for (final Book b : books1) {
+            System.out.println(b);
+        }
+        System.out.println("\n----\n");
+        session1.close();
         factory.close();
     }
 }
